60cbda4234a5ddab94502eab44cd67c38a0d2d0e
Igor Malinavichus <crazych4tty@gmail.com>
2023-08-04 23:32:25 +0300

    feat: Сверстать footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,9 +2,18 @@ import React from "react";
 import './Footer.css';
 
 function Footer() {
-    return (
-      <></>
-    );
-  }
-  
-  export default Footer;
+  return (
+    <footer className="footer">
+      <p className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className="footer__container">
+        <p className="footer__copyright">&copy; {new Date().getFullYear()}</p>
+        <div className="footer__links">
+          <a href="https://practicum.yandex.ru" target="_blank" className="footer__link" rel="noreferrer">Яндекс.Практикум</a>
+          <a href="https://github.com/Raneren" target="_blank" className="footer__link" rel="noreferrer">Github</a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
