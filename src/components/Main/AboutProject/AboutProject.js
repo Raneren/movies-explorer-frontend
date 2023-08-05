39b16a4a1acdb429eb93090fa9200e6e6c7c771d
Igor Malinavichus <crazych4tty@gmail.com>
2023-08-05 16:12:33 +0300

    feat: Сверстать AboutProject

--- src/components/Main/AboutProject/AboutProject.js
+++ src/components/Main/AboutProject/AboutProject.js
@@ -3,8 +3,30 @@ import './AboutProject.css';
 
 function AboutProject() {
     return (
-      <></>
+        <div className="about-poject">
+            <h2 className="about-poject__title">О проекте</h2>
+            <div className="about-poject__text-container">
+                <div className="about-poject__column">
+                    <p className="about-poject__column-title">Дипломный проект включал 5 этапов</p>
+                    <p className="about-poject__paragraph">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+                </div>
+                <div className="about-poject__column">
+                    <p className="about-poject__column-title">На выполнение диплома ушло 5 недель</p>
+                    <p className="about-poject__paragraph">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+                </div>
+            </div>
+            <div className="about-poject__scheme">
+                <div className="about-poject__scheme-back">
+                    <p className="about-poject__scheme-element about-poject__scheme-element_back">1 неделя</p>
+                    <p className="about-poject__scheme-subtitle">Back-end</p>
+                </div>
+                <div className="about-poject__scheme-front">
+                    <p className="about-poject__scheme-element about-poject__scheme-element_front">4 недели</p>
+                    <p className="about-poject__scheme-subtitle">Front-end</p>
+                </div>
+            </div>
+        </div>
     );
-  }
-  
-  export default AboutProject;
+}
+
+export default AboutProject;
