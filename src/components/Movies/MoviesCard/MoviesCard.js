0403a262a274f28f1eaca1d756cf17084f7609db
Igor Malinavichus <crazych4tty@gmail.com>
2023-08-06 00:07:02 +0300

    feat: Сверстать MoviesCardList и MoviesCard

--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -1,10 +1,17 @@
 import React from "react";
 import './MoviesCard.css';
+import moviePreview from "../../../images/movie-test-1.png"
 
 function MoviesCard() {
     return (
-      <></>
+        <div className="movies-card">
+            <img src={moviePreview} alt=""></img>
+            <div className="movies-card__info">
+                <p className="movies-card__name">33 слова о дизайне</p>
+                <p className="movies-card__duration">1ч 17м</p>
+            </div>
+        </div>
     );
-  }
-  
-  export default MoviesCard;
+}
+
+export default MoviesCard;
--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -1,10 +1,27 @@
 import React from "react";
 import './MoviesCardList.css';
+import MoviesCard from "../MoviesCard/MoviesCard";
 
 function MoviesCardList() {
     return (
-      <></>
+        <div className="movies-card-list">
+            <div className="movies-card-list__container">
+                <MoviesCard />
+                <MoviesCard />
+                <MoviesCard />
+                <MoviesCard />
+                <MoviesCard />
+                <MoviesCard />
+                <MoviesCard />
+                <MoviesCard />
+                <MoviesCard />
+                <MoviesCard />
+                <MoviesCard />
+                <MoviesCard />
+            </div>
+            <button className="movies-card-list__button" type="button">Ещё</button>
+        </div>
     );
-  }
-  
-  export default MoviesCardList;
+}
+
+export default MoviesCardList;
