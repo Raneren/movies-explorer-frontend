f120b6011c3e1883fcc3cb5f47592344325a2eaa
Igor Malinavichus <crazych4tty@gmail.com>
2023-08-05 14:18:45 +0300

    feat: Сверстать header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,26 @@
 import React from "react";
+import logo from "../../images/logo.svg";
 import './Header.css';
 
 function Header() {
-    return (
-      <></>
-    );
-  }
-  
-  export default Header;
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} alt="Логотип" />
+      <div className="header__navigation">
+      </div>
+      <div className="header__buttons">
+        <button
+          type="button"
+          className="header__button_type_signup"
+        >Регистрация</button>
+        <button
+          type="button"
+          className="header__button_type_signin"
+        >Войти</button>
+      </div>
+
+    </header>
+  );
+}
+
+export default Header;
