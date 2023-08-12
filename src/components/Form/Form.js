a002ada48f8e375d2f9f3222bf0ce9ef8f7a87e8
Igor Malinavichus <crazych4tty@gmail.com>
2023-08-12 12:19:48 +0300

    feat: Сверстать Login

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,16 +5,20 @@ import "./Form.css";
 function Form(props) {
   return (
     <form className="form" name={props.formName} method="post">
-      <h2 className="form__title">{props.title}</h2>
-      {props.children}
-      <button type="submit" className="form__submit-btn">
-        {props.buttonText}
-      </button>
-      <div className="form__link-container">
-        <p className="form__link-description">{props.linkDescription}</p>
-        <Link to={props.linkPatch} className="form__link">
-          {props.linkText}
-        </Link>
+      <div className="form__top-container">
+        <h2 className="form__title">{props.title}</h2>
+        {props.children}
+      </div>
+      <div className="form__bottom-container">
+        <button type="submit" className="form__submit-btn">
+          {props.buttonText}
+        </button>
+        <div className="form__link-container">
+          <p className="form__link-description">{props.linkDescription}</p>
+          <Link to={props.linkPatch} className="form__link">
+            {props.linkText}
+          </Link>
+        </div>
       </div>
     </form>
   );
