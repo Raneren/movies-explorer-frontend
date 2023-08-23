e58bce3b0b02b9760e3fbc44bdac194c7e6ac042
Igor Malinavichus <crazych4tty@gmail.com>
2023-08-23 21:39:59 +0300

    feat: Реализовать работу пагинации

--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -1,19 +1,48 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
 import "./MoviesCardList.css";
 import MoviesCard from "../MoviesCard/MoviesCard";
+import { useMediaQuery } from "../../../hooks/useMediaQuery";
 
 function MoviesCardList(props) {
-  const location = useLocation();
+  const isDesktop = useMediaQuery("(min-width: 1280px)");
+  const isTablet = useMediaQuery("(min-width: 650px)");
+
+  const initialMovieCount = isDesktop ? 12 : isTablet ? 8 : 5;
+
+  const [visibleMovieCount, setVisibleMovieCount] =
+    React.useState(initialMovieCount);
+
+  function handleClick() {
+    calculateMovieCount();
+  }
+
+  function calculateMovieCount() {
+    if (isDesktop) {
+      return setVisibleMovieCount(visibleMovieCount + 3);
+    }
+
+    if (isTablet) {
+      return setVisibleMovieCount(visibleMovieCount + 2);
+    }
+
+    setVisibleMovieCount(visibleMovieCount + 2);
+  }
+  console.log(props.movies.length <= visibleMovieCount);
   return (
     <div className="movies-card-list">
       <div className="movies-card-list__container">
-        {props.movies.map((item) => (
+        {props.movies?.slice(0, visibleMovieCount).map((item) => (
           <MoviesCard movie={item} key={item.id} />
         ))}
       </div>
-      {location.pathname === "/movies" && (
-        <button className="movies-card-list__button" type="button">
+      {props.movies.length <= visibleMovieCount ? (
+        ""
+      ) : (
+        <button
+          className="movies-card-list__button"
+          type="button"
+          onClick={handleClick}
+        >
           Ещё
         </button>
       )}
